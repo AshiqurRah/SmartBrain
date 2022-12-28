7c16c512d55ec6cc8a91525e831ebea6f2c9e6e5
Ashiqur Rahman <e0689695@u.nus.edu>
2022-12-28 22:48:30 +0800

    Update app

--- src/App.js
+++ src/App.js
@@ -25,9 +25,27 @@ class App extends Component {
       imageUrl:'',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: '' // js will create new date when joined gets executed
+      }
     }
   }
+
+  loadUser = (data) => {
+    this.setState({user : {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined // js will create new date when joined gets executed
+    }})
+  }
+
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -77,6 +95,7 @@ class App extends Component {
           })
             .then(response => response.json())
             .then(count => {
+              // Object.assign is used for objects and easier assignment
               this.setState(Object.assign(this.state.user, { entries: count}))
             })
 
@@ -107,7 +126,10 @@ class App extends Component {
         { route === 'home' 
           ? <div>
               <Logo/>
-              <Rank />
+              <Rank 
+                name = {this.state.user.name}
+                entries = {this.state.user.entries}
+              />
               <ImageLinkForm 
                 onInputChange = {this.onInputChange} 
                 onButtonSubmit= {this.onButtonSubmit}
@@ -123,6 +145,7 @@ class App extends Component {
                 onRouteChange = {this.onRouteChange}
               />
             : <Register
+                loadUser = {this.loadUser}
                 onRouteChange = {this.onRouteChange}
               />
           )
